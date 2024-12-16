6fde92e099b1e520aa3c1a94a00f9b2f3991e762
Kirill Pashnin <kirillpashnin.dev@gmail.com>
2024-12-16 22:29:21 +0100

    Added better input asking and checkWin function

--- app.js
+++ app.js
@@ -2,12 +2,7 @@
 //Create gameboard iife
 //Create player factory
 //Add logic to check if a player won
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
+const readline = require("readline-sync");
 
 const Gameboard = (function () {
   const gameboard = [
@@ -16,6 +11,24 @@ const Gameboard = (function () {
     [null, null, null]
   ];
   return {
+    checkWinner: function (playerSymbol) {
+      for (let i = 0; i < 3; i++) {
+        //Check rows for win
+        if (board[i].every(cell => cell === playerSymbol)) {
+          currentPlayer.addPoints();
+        }
+        //Checks columns
+        if (board[0][i] === playerSymbol && board[1][i] === playerSymbol && board[2][i] === playerSymbol) {
+          currentPlayer.addPoints();
+        }
+        if (board[0][0] === playerSymbol && board[1][1] === playerSymbol && board[2][2] === playerSymbol) {
+          currentPlayer.addPoints();
+        } // Diagonal 1
+        if (board[0][2] === playerSymbol && board[1][1] === playerSymbol && board[2][0] === playerSymbol) {
+          currentPlayer.addPoints();
+        }; // Diagonal 2
+      }
+    },
     getBoard: function () {
       return gameboard;
     },
@@ -54,18 +67,23 @@ function Player(symbol) {
 
 
 function playRound() {
-  rl.question("Input the symbol you want to play with X or O: ", (symbol) => {
-    const player1 = Player(symbol);
-    Gameboard.display();
-    while (player1.getPoints != 3) {
-      rl.question("Input the row from 1 to 3:  ", (row) => {
-        rl.question("Input the column from 1 to 3: ", (column) => {
-          Gameboard.makeMove(row, column)
-        })
-      })
+  const symbol = readline.question("What symbol you want to play with? X or O").toUpperCase();
+  if (symbol != "X" && symbol != "O") {
+    console.log("Invalid symbol, pick one of the two allowed symbols.");
+    return playRound();
+  }
+  const player1 = Player(symbol);
+  while (player1.getPoints != 1) {
+    const row = parseInt(readline.question("Choose the row from 1 - 3")) - 1;
+    const column = parseInt(readline.question("Choose the column from 1 -3")) - 1;
+    if (Gameboard.makeMove(row, column, player1.symbol)) {
+      Gameboard.display();
     }
-  });
-}
+    else {
+      console.log("Invalid move");
+    }
+  }
+};
 
 playRound();
 
